a4e531f8cdbed564133fea3d63dd731825e56341
Alberto Campos <albertocamposhdez@hotmail.com>
2020-09-22 19:32:47 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+const state = {
+  notes: [
+    {
+      id: 1,
+      note: "Hello World",
+    },
+  ],
+};
 function App() {
+  const { notes } =  state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AmplifySignOut />
+      <div className="flex flex-column items-center justify-content pa3 bg-washed-red">
+        <h1 className="code f2-l">Amplify Notetaker</h1>
+
+        <form className="mb3">
+          <input
+            className="pa2 f4"
+            type="text"
+            placeholder="Write your note"
+          ></input>
+          <button className="pa2 f4" type="submit">
+            Add note
+          </button>
+        </form>
+        <div>{notes.map(item=>(
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">
+              {item.note}
+            </li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}</div>
+      </div>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
